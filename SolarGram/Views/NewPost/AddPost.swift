2cf743b4fa590fe986f68f21c001d4120deca0d6
rachelqian777 <jiaqiqia@usc.edu>
2023-04-18 16:10:13 -0700

    0418/0407PM
    Created a new branch from main and transferred the changes, posting with both camera and photo album should work now.

--- SolarGram/Views/NewPost/AddPost.swift
+++ SolarGram/Views/NewPost/AddPost.swift
@@ -4,133 +4,104 @@
 //
 //  Created by Rachel Qian on 4/15/23.
 //
-
 import SwiftUI
 
 struct AddPost: View {
     @Environment(\.dismiss) var dismiss
-
     @EnvironmentObject var viewModel: ViewModel
-    @State private var showCameraPicker = false
-    @State private var showImagePicker = false
+    @State var showCameraPicker = false
+    @State var showImagePicker = false
+    @Binding var showNext: Bool
+                
+    var body: some View {
+        
+        VStack {
+            //The button for taking a picture
+            Button("Take A Picture") {
+                showCameraPicker.toggle()
+            }
+            .padding()
+            .background(Color.black)
+            .foregroundColor(Color.white)
+            .cornerRadius(40)
+            
+            //The button for adding a picture from the album
+            
+            Button("Upload From Album") {
+                showImagePicker.toggle()
+            }
+            .padding()
+            .background(Color.black)
+            .foregroundColor(Color.white)
+            .cornerRadius(40)
+            
+        }
+        .fullScreenCover(isPresented: $showCameraPicker, content: {
+            PickerView(isPresented: $showCameraPicker, type: .camera)
+        })
+        .sheet(isPresented: $showImagePicker, content: {
+            PickerView(isPresented: $showImagePicker, type: .image)
+        })
+    }
+}
+struct PickerView: View {
+    @EnvironmentObject var viewModel: ViewModel
     
-//    @State var isPresentNewPost : Bool = false
+    @State var showNextView = false
     
     @Binding var isPresented: Bool
-
     
-
-//    var body: some View {
-        
-        
-//        NavigationView {
-//            VStack{
-//                //The button for taking a picture
-//                Button("Take A Picture") {
-//                    showImagePicker.toggle()
-//                }
-//                .padding()
-//                .background(Color.black)
-//                .foregroundColor(Color.white)
-//                .cornerRadius(40)
-//                .fullScreenCover(isPresented: $showImagePicker, content: {
-//                    CameraPicker(viewModel: viewModel, isPresentNewPost: .constant(false))
-//                })
-//
-//
-//                NavigationLink{
-//                    VStack {
-//                        ImagePicker(viewModel: viewModel, isPresentNewPost: .constant(false))
-//                        Button(action: {
-//                            self.isPresentNewPost.toggle()
-//                        }) {
-//                            Text("Add Selected Image")
-//                                .padding()
-//                                .background(Color.black)
-//                                .foregroundColor(Color.white)
-//                                .cornerRadius(40)
-//                        }.sheet(isPresented: $isPresentNewPost) {
-//                            NewPostView(isPresentNewPost: .constant(false))
-//                        }
-//                    }
-//
-//                } label: {
-//                    Text("Upload From Album")
-//                        .padding()
-//                        .background(Color.black)
-//                        .foregroundColor(Color.white)
-//                        .cornerRadius(40)
-//                }
-//
-//
-//            }
-//        }
-//    }
+    var type: PickerType
     
-    
-    var body: some View {
-            
-            VStack {
-                //The button for taking a picture
-                Button("Take A Picture") {
-                    showCameraPicker.toggle()
-                }
-                .padding()
-                .background(Color.black)
-                .foregroundColor(Color.white)
-                .cornerRadius(40)
-                
-                //The button for adding a picture from the album
-                
-                Button("Upload From Album") {
-                    showImagePicker.toggle()
-                    isPresented = true
-                }
-                .padding()
-                .background(Color.black)
-                .foregroundColor(Color.white)
-                .cornerRadius(40)
-                
-            }
-            .fullScreenCover(isPresented: $showCameraPicker, content: {
-                cameraStack
-            })
-            .sheet(isPresented: $showImagePicker, content: {
-                imageStack
-            })
-
+    enum PickerType {
+        case camera, image
     }
     
-    
-    var imageStack: some View {
-            NavigationStack {
-                ImagePicker(viewModel: viewModel, isPresentNewPost: .constant(false))
-                
-                NavigationLink {
-                    NewPostView(isPresentNewPost: .constant(false), isPresented: $showImagePicker)
-                } label: {
-                    Text("Add Selected Image")
-                }
-            }
+    var body: some View {
+        switch type {
+            case .camera: cameraStack
+            case .image: imageStack
+        }
     }
     
     var cameraStack: some View {
         NavigationStack {
-            CameraPicker(viewModel: viewModel, isPresentNewPost: .constant(false))
+            CameraPicker(viewModel: viewModel, showNext: $showNextView)
+                .edgesIgnoringSafeArea([.top, .bottom])
             
-            NavigationLink {
-                NewPostView(isPresentNewPost: .constant(false), isPresented: .constant(false))
-            } label: {
-                
-            }
+                .navigationDestination(isPresented: $showNextView) {
+                    NewPostView(isPresented: $isPresented)
+                        .navigationBarBackButtonHidden() // the camera cannot refresh itself and the user would get stuck if they go back
+                    
+                    // instead have a cancel button in case user does not want to post
+                        .toolbar {
+                            ToolbarItem(placement: .cancellationAction) {
+                                Button("Cancel", role: .cancel) {
+                                    isPresented = false
+                                }
+                            }
+                        }
+                    
+                }
         }
     }
     
+    var imageStack: some View {
+        NavigationStack {
+            ImagePicker(viewModel: viewModel)
+            
+            NavigationLink {
+                NewPostView(isPresented: $isPresented)
+            } label: {
+                Text("Add Selected Image")
+            }
+        }
+    }
 }
 
 struct AddPost_Previews: PreviewProvider {
     static var previews: some View {
-        AddPost(isPresented: .constant(false))
+        AddPost(showNext: .constant(false))
             .environmentObject(ViewModel())
     }
 }
